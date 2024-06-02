aa389498a82e424848641984dd52ced48f4277bf
Stan Manilov <stanislav.manilov@gmail.com>
2024-06-02 14:25:45 +0300

    Enter OOP
    Introducing a type to keep the state of the parser allows the
    abstracting of the handle_event function and reusing it in places, where
    it parse.next() is used, but the result is discarded if it's not what's
    expected.
    
    This is an improvement, although it does not fix the text. See next
    commit for fix.

--- src/gpx_reader.rs
+++ src/gpx_reader.rs
@@ -2,7 +2,11 @@
 use chrono::{DateTime, Utc};
 use std::fs::File;
 use std::io::BufReader;
-use xml::reader::{EventReader, XmlEvent};
+use std::mem;
+use xml::{
+    attribute::OwnedAttribute,
+    reader::{EventReader, Result, XmlEvent},
+};
 
 #[derive(Debug, PartialEq)]
 pub struct GpxPoint {
@@ -17,115 +21,195 @@ pub struct GpxPoint {
 pub fn read_gpx(filename: &str) -> Vec<GpxPoint> {
     let file = File::open(filename).expect("Unable to open file");
     let file = BufReader::new(file);
-    parse_gpx(file)
+    GpxParser::new().parse_gpx(file)
 }
 
-fn parse_gpx<R: std::io::Read>(file: BufReader<R>) -> Vec<GpxPoint> {
-    let mut parser = EventReader::new(file);
+struct GpxParser {
+    current_point: GpxPoint,
+    points: Vec<GpxPoint>,
+    inside_trkpt: bool,
+    inside_extensions: bool,
+}
 
-    let mut points = Vec::new();
-    let mut current_point = GpxPoint {
-        lat: 0.0,
-        lon: 0.0,
-        elevation: None,
-        time: None,
-        heart_rate: None,
-        cadence: None,
-    };
-    let mut inside_trkpt = false;
-    let mut inside_extensions = false;
+impl GpxParser {
+    fn new() -> GpxParser {
+        GpxParser {
+            current_point: GpxPoint {
+                lat: 0.0,
+                lon: 0.0,
+                elevation: None,
+                time: None,
+                heart_rate: None,
+                cadence: None,
+            },
+            points: Vec::new(),
+            inside_trkpt: false,
+            inside_extensions: false,
+        }
+    }
 
-    loop {
-        let event = parser.next();
+    fn parse_gpx<R: std::io::Read>(mut self, file: BufReader<R>) -> Vec<GpxPoint> {
+        let mut parser = EventReader::new(file);
+
+        loop {
+            let event = parser.next();
+            if !self.handle_event(&mut parser, event) {
+                break;
+            }
+        }
+
+        self.points
+    }
+
+    fn handle_trkpt(&mut self, attributes: Vec<OwnedAttribute>) -> bool {
+        self.inside_trkpt = true;
+        for attr in attributes {
+            match attr.name.local_name.as_str() {
+                "lat" => self.current_point.lat = attr.value.parse().unwrap(),
+                "lon" => self.current_point.lon = attr.value.parse().unwrap(),
+                _ => {}
+            }
+        }
+        true
+    }
+
+    fn handle_event<R: std::io::Read>(
+        &mut self,
+        mut parser: &mut EventReader<R>,
+        event: Result<XmlEvent>,
+    ) -> bool {
         match event {
             Ok(XmlEvent::StartElement {
                 name, attributes, ..
             }) => match name.local_name.as_str() {
-                "trkpt" => {
-                    inside_trkpt = true;
-                    for attr in attributes {
-                        match attr.name.local_name.as_str() {
-                            "lat" => current_point.lat = attr.value.parse().unwrap(),
-                            "lon" => current_point.lon = attr.value.parse().unwrap(),
-                            _ => {}
-                        }
-                    }
-                }
-                "ele" => {
-                    if inside_trkpt {
-                        if let Ok(XmlEvent::Characters(elevation)) = parser.next() {
-                            match elevation.parse() {
-                                Ok(elevation) => current_point.elevation = Some(elevation),
-                                Err(err) => println!("warning: could not parse elevation: {err}"),
-                            };
-                        }
-                    }
-                }
-                "time" => {
-                    if inside_trkpt {
-                        if let Ok(XmlEvent::Characters(time)) = parser.next() {
-                            match time.parse::<DateTime<Utc>>() {
-                                Ok(time) => current_point.time = Some(time),
-                                Err(err) => println!("warning: could not parse datetime: {err}"),
-                            }
-                        }
-                    }
-                }
-                "extensions" => {
-                    if inside_trkpt {
-                        inside_extensions = true;
-                    }
-                }
-                "gpxtpx:hr" => {
-                    if inside_extensions {
-                        if let Ok(XmlEvent::Characters(hr)) = parser.next() {
-                            match hr.parse() {
-                                Ok(heart_rate) => current_point.heart_rate = Some(heart_rate),
-                                Err(err) => println!("warning: could not parse heart_rate: {err}"),
-                            };
-                        }
-                    }
-                }
-                "gpxtpx:cad" => {
-                    if inside_extensions {
-                        if let Ok(XmlEvent::Characters(cadence)) = parser.next() {
-                            match cadence.parse() {
-                                Ok(cadence) => current_point.cadence = Some(cadence),
-                                Err(err) => println!("warning: could not parse cadence: {err}"),
-                            };
-                        }
-                    }
-                }
-                _ => {}
+                "trkpt" => self.handle_trkpt(attributes),
+                "ele" => self.handle_ele(&mut parser),
+                "time" => self.handle_time(&mut parser),
+                "extensions" => self.handle_extensions(),
+                "gpxtpx:hr" => self.handle_hr(&mut parser),
+                "gpxtpx:cad" => self.handle_cad(&mut parser),
+                _ => true,
             },
             Ok(XmlEvent::EndElement { name }) => match name.local_name.as_str() {
-                "trkpt" => {
-                    inside_trkpt = false;
-                    points.push(current_point);
-                    current_point = GpxPoint {
-                        lat: 0.0,
-                        lon: 0.0,
-                        elevation: None,
-                        time: None,
-                        heart_rate: None,
-                        cadence: None,
-                    };
-                }
-                "extensions" => {
-                    inside_extensions = false;
-                }
-                _ => {}
+                "trkpt" => self.handle_end_trkpt(),
+                "extensions" => self.handle_end_extensions(),
+                _ => true,
             },
-            Ok(XmlEvent::EndDocument) => break,
+            Ok(XmlEvent::EndDocument) => false,
             Err(e) => {
                 println!("Error: {}", e);
-                break;
+                false
             }
-            _ => {}
+            _ => true,
         }
     }
 
-    points
+    fn handle_end_trkpt(&mut self) -> bool {
+        self.inside_trkpt = false;
+        let mut new_point = GpxPoint {
+            lat: 0.0,
+            lon: 0.0,
+            elevation: None,
+            time: None,
+            heart_rate: None,
+            cadence: None,
+        };
+        mem::swap(&mut self.current_point, &mut new_point);
+        self.points.push(new_point);
+        true
+    }
+
+    fn handle_end_extensions(&mut self) -> bool {
+        self.inside_extensions = false;
+        true
+    }
+
+    fn handle_ele<R: std::io::Read>(&mut self, parser: &mut EventReader<R>) -> bool {
+        if self.inside_trkpt {
+            let event = parser.next();
+            if let Ok(XmlEvent::Characters(elevation)) = &event {
+                match elevation.parse() {
+                    Ok(elevation) => self.current_point.elevation = Some(elevation),
+                    Err(err) => {
+                        println!("warning: could not parse elevation: {err}")
+                    }
+                };
+                true
+            } else {
+                self.handle_event(parser, event)
+            }
+        } else {
+            println!("warning: ele outside trkpt");
+            true
+        }
+    }
+
+    fn handle_time<R: std::io::Read>(&mut self, parser: &mut EventReader<R>) -> bool {
+        if self.inside_trkpt {
+            let event = parser.next();
+            if let Ok(XmlEvent::Characters(time)) = &event {
+                match time.parse::<DateTime<Utc>>() {
+                    Ok(time) => self.current_point.time = Some(time),
+                    Err(err) => {
+                        println!("warning: could not parse datetime: {err}")
+                    }
+                }
+                true
+            } else {
+                self.handle_event(parser, event)
+            }
+        } else {
+            println!("warning: time outside trkpt");
+            true
+        }
+    }
+
+    fn handle_extensions(&mut self) -> bool {
+        if self.inside_trkpt {
+            self.inside_extensions = true;
+        }
+        true
+    }
+
+    fn handle_hr<R: std::io::Read>(&mut self, parser: &mut EventReader<R>) -> bool {
+        if self.inside_extensions {
+            let event = parser.next();
+            if let Ok(XmlEvent::Characters(hr)) = &event {
+                match hr.parse() {
+                    Ok(heart_rate) => self.current_point.heart_rate = Some(heart_rate),
+                    Err(err) => {
+                        println!("warning: could not parse heart_rate: {err}")
+                    }
+                };
+                true
+            } else {
+                self.handle_event(parser, event)
+            }
+        } else {
+            println!("warning: hr outside extensions");
+            true
+        }
+    }
+
+    fn handle_cad<R: std::io::Read>(&mut self, parser: &mut EventReader<R>) -> bool {
+        if self.inside_extensions {
+            let event = parser.next();
+            if let Ok(XmlEvent::Characters(cadence)) = &event {
+                match cadence.parse() {
+                    Ok(cadence) => self.current_point.cadence = Some(cadence),
+                    Err(err) => {
+                        println!("warning: could not parse cadence: {err}")
+                    }
+                };
+                true
+            } else {
+                self.handle_event(parser, event)
+            }
+        } else {
+            println!("warning: cad outside extensions");
+            true
+        }
+    }
 }
 
 #[cfg(test)]
@@ -152,7 +236,7 @@ mod tests {
         "#;
         let cursor = Cursor::new(gpx_data);
         let reader = BufReader::new(cursor);
-        let points = parse_gpx(reader);
+        let points = GpxParser::new().parse_gpx(reader);
 
         let expected_point = GpxPoint {
             lat: 44.4705880,
